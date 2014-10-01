21d775c95f360cdfd7b800c2a3fc4f530aaab9d1
Kai Kewley <kaikewley@gmail.com>
2014-10-01 10:16:42 +0100

    Reflect branch changes in concept children list.

--- data-service/src/main/java/org/ihtsdo/snomed/server/dataservice/ConceptService.java
+++ data-service/src/main/java/org/ihtsdo/snomed/server/dataservice/ConceptService.java
@@ -4,17 +4,29 @@ import org.ihtsdo.snomed.server.dataservice.json.JsonComponentMerge;
 import org.ihtsdo.snomed.server.dataservice.json.JsonComponentMergeException;
 import org.ihtsdo.snomed.server.dataservice.json.OrderedConceptJsonObject;
 import org.ihtsdo.snomed.server.dataservice.util.StreamUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ConceptService {
 
 	public static final String MASTER = "master";
+
 	private final JsonComponentMerge jsonComponentMerge;
+	private final Set<String> conceptToChildSummaryAttributeMapping;
 
 	public ConceptService() {
 		jsonComponentMerge = new JsonComponentMerge();
+
+		conceptToChildSummaryAttributeMapping = new HashSet<>();
+		conceptToChildSummaryAttributeMapping.add("defaultTerm");
+		conceptToChildSummaryAttributeMapping.add("definitionStatus");
+		conceptToChildSummaryAttributeMapping.add("active");
+		conceptToChildSummaryAttributeMapping.add("module");
 	}
 
 	public String loadConcept(String conceptId, String branch) throws IOException, JsonComponentMergeException {
@@ -37,10 +49,25 @@ public class ConceptService {
 		String conceptChildren = getConceptChildrenString(conceptId, MASTER);
 
 		if (!branch.equals(MASTER)) {
-			String branchDelta = getConceptChildrenString(conceptId, getBranchPath(branch));
-			if (branchDelta != null) {
-				conceptChildren = jsonComponentMerge.mergeComponent(conceptChildren, branchDelta);
+			String branchPath = getBranchPath(branch);
+
+			// Merge in branch changes. Active
+			JSONArray conceptSummaries = new JSONArray(conceptChildren);
+			for (int a = 0; a < conceptSummaries.length(); a++) {
+				JSONObject childConceptSummary = conceptSummaries.getJSONObject(a);
+				String childConceptSummaryId = childConceptSummary.getString("conceptId");
+				String childConceptString = getConceptString(childConceptSummaryId, branchPath);
+				if (childConceptString != null) {
+					// This concept is modified in this branch. Let's apply any relevant changes.
+					JSONObject childConcept = new JSONObject(childConceptString);
+					for (String attributeName : conceptToChildSummaryAttributeMapping) {
+						if (childConcept.has(attributeName)) {
+							childConceptSummary.put(attributeName, childConcept.get(attributeName));
+						}
+					}
+				}
 			}
+			conceptChildren = conceptSummaries.toString();
 		}
 		return conceptChildren;
 	}
--- data-service/src/test/java/org/ihtsdo/snomed/server/dataservice/ConceptServiceTest.java
+++ data-service/src/test/java/org/ihtsdo/snomed/server/dataservice/ConceptServiceTest.java
@@ -1,9 +1,12 @@
 package org.ihtsdo.snomed.server.dataservice;
 
+import org.ihtsdo.snomed.server.dataservice.json.JsonComponentMergeException;
 import org.ihtsdo.snomed.server.dataservice.util.StreamUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
+
+import java.io.IOException;
 
 public class ConceptServiceTest {
 
@@ -18,17 +21,36 @@ public class ConceptServiceTest {
 	public void testLoadConceptOnMaster() throws Exception {
 		String expectedMasterConcept = StreamUtils.readStream(getClass().getResourceAsStream("431371001-master.json"));
 
-		String masterConcept = conceptService.loadConcept("431371001", "master");
+		String masterConcept = conceptService.loadConcept("431371001", ConceptService.MASTER);
 
 		JSONAssert.assertEquals(expectedMasterConcept, masterConcept, true);
 	}
 
 	@Test
 	public void testLoadConceptOnBranch() throws Exception {
-		String expectedMasterConcept = StreamUtils.readStream(getClass().getResourceAsStream("431371001-authoring-team-branch.json"));
+		String expectedBranchConcept = StreamUtils.readStream(getClass().getResourceAsStream("431371001-authoring-team-branch.json"));
 
-		String masterConcept = conceptService.loadConcept("431371001", "authoring-team");
+		String branchConcept = conceptService.loadConcept("431371001", "authoring-team");
 
-		JSONAssert.assertEquals(expectedMasterConcept, masterConcept, true);
+		JSONAssert.assertEquals(expectedBranchConcept, branchConcept, true);
 	}
+
+	@Test
+	public void testLoadConceptChildrenOnMaster() throws IOException, JsonComponentMergeException {
+		String expectedConceptChildren = StreamUtils.readStream(getClass().getResourceAsStream("410607006-children-master.json"));
+
+		String conceptChildren = conceptService.loadConceptChildren("410607006", ConceptService.MASTER);
+
+		JSONAssert.assertEquals(expectedConceptChildren, conceptChildren, true);
+	}
+
+	@Test
+	public void testLoadConceptChildrenOnBranch() throws IOException, JsonComponentMergeException {
+		String expectedConceptChildren = StreamUtils.readStream(getClass().getResourceAsStream("410607006-children-branch.json"));
+
+		String conceptChildren = conceptService.loadConceptChildren("410607006", "authoring-team");
+
+		JSONAssert.assertEquals(expectedConceptChildren, conceptChildren, true);
+	}
+
 }
